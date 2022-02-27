1f16e3bf891a053be90d8fae09485d8a2ff4bdab
EliasMura <elias-murat@outlook.com>
2022-02-26 21:27:35 -0300

    finish demo

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,18 @@ const navToggleFunc = () => nav.classList.toggle('active');
 
 navMenuBtn.addEventListener('click', navToggleFunc);
 navCloseBtn.addEventListener('click', navToggleFunc);
+
+// theme toggle variables
+const themeBtn = document.querySelectorAll('.theme-btn');
+
+for (let index = 0; index < themeBtn.length; index++) {
+  themeBtn[index].addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    for (let index = 0; index < themeBtn.length; index++) {
+      themeBtn[index].classList.toggle('light');
+      themeBtn[index].classList.toggle('dark');
+    }
+  });
+}
